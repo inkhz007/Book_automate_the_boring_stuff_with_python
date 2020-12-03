85233e6421e4ada56601b691c3df380a73150c75
ink <ink_hz@163.com>
2020-12-03 23:39:36 +0800

    [Add] add chapter 1 python basics practice code

---
+++ Src/Chapter 1  Python Basics/Basic.py
@@ -0,0 +1,22 @@
+print(22//8)
+print(22/8)
+
+# print type
+type(8)
+type(8.0)
+type("hello")
+
+# 字符串连接和复制
+print("hello" + " " + "world")
+print("hello"*3)
+
+# 第一个程序
+print('Hello World !')
+print('what is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+print('what is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
